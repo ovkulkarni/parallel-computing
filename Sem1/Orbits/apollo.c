e8f84105982376ed9ac7ee2e95d0f911bd11789d
Omkar Kulkarni <ovkulkarni@users.noreply.github.com>
2018-01-12 11:09:42 -0500

    stuff

--- Sem1/Orbits/apollo.c
+++ Sem1/Orbits/apollo.c
@@ -7,7 +7,7 @@
 #define MR 1737E3
 #define MM 7.34767309E22
 #define MOH 384402E3
-#define MV 500
+#define MV 1022
 #define SD ER + (MOH / 2)
 #define APV 1500
 #define PI 3.14159265358979323846
@@ -40,10 +40,10 @@ int runSim(float theta, int print){
   float apax = apmx + apex;
   float apay = apmy + apey;
   if(print)
-    printf("t\tv\tr\n");
-  for(float t = dt; t < tot; t += dt){
+    printf("t\tx\ty\tmx\tmy\n");
+  for(float t = dt; t < (tot*(1+(2*print))); t += dt){
       if(print)
-        printf("%0.2f\t%0.2f\t%0.2f\n", t, hypot(apvx, apvy), aprm);
+        printf("%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\n", t, apx, apy, mx, my);
       mr = hypot(mx, my);
       ma = (G*EM)/pow(mr, 2);
       max = -1 * ma * (mx / mr);
@@ -98,8 +98,7 @@ float search(float min, float max){
 }
 int main(int argc, char* argv[]){
     //fprintf(stderr, "Returned %d\n", runSim(0.69, 1));
-    runSim(0.56f, 1);
-    /*float theta = search(0.0f, 0.75f);
+    float theta = search(0.0f, 0.75f);
     fprintf(stderr, "Found angle: %0.2f\n", theta);
     /**/
     return 0;
